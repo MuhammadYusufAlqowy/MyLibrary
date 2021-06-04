b21bea3e61fafe23723edc408cbae4fb3688a263
MuhammadYusufAlqowy <37199795+MuhammadYusufAlqowy@users.noreply.github.com>
2021-06-04 16:32:07 +0700

    Update *Fix BaseActivity.kt

--- mylibrary/src/main/java/com/yadev/mylibrary/activity/BaseActivity.kt
+++ mylibrary/src/main/java/com/yadev/mylibrary/activity/BaseActivity.kt
@@ -1,14 +1,20 @@
 package com.yadev.mylibrary.activity
 
+import android.content.Context
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import androidx.appcompat.app.AppCompatActivity
 import androidx.viewbinding.ViewBinding
 import com.yadev.mylibrary.R
+import com.yadev.mylibrary.hideSoftKeyboard
 import com.yadev.mylibrary.makeStatusBarTransparent
 
 abstract class BaseActivity<V: ViewBinding>(val fullScreen: Boolean = false) : AppCompatActivity(){
     lateinit var layout : V
+    private var dispatch = false
+    private var fontScale = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (fullScreen){
@@ -21,4 +27,30 @@ abstract class BaseActivity<V: ViewBinding>(val fullScreen: Boolean = false) : A
 
     }
     abstract fun getBindingView() : V
+
+    fun enableDispatch(status: Boolean) {
+        dispatch = status
+    }
+
+    fun disbleFontScale(status: Boolean){
+        fontScale = status
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+        if (dispatch) {
+            currentFocus?.let {
+                hideSoftKeyboard()
+            }
+        }
+        return super.dispatchTouchEvent(ev)
+    }
+
+    override fun attachBaseContext(newBase: Context?) {
+        super.attachBaseContext(newBase)
+        if (fontScale){
+            val newOverride = Configuration(newBase?.resources?.configuration)
+            newOverride.fontScale = 1.0f
+            applyOverrideConfiguration(newOverride)
+        }
+    }
 }
